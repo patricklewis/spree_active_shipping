f4725e7432fdb41d2ddb5df6e89b8bfc3e5aa9e9
Sasha Gofman <gofmanaa@gmail.com>
2017-07-25 17:39:05 +0300

    Create freight_priority.rb

---
+++ app/models/spree/calculator/shipping/fedex/freight_priority.rb
@@ -0,0 +1,11 @@
+module Spree
+  module Calculator::Shipping
+    module Fedex
+      class FreightPriority < Spree::Calculator::Shipping::Fedex::Base
+        def self.description
+           I18n.t("fedex.freight_priority")
+        end
+      end
+    end
+  end
+end
