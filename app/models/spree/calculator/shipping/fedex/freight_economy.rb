b94657a885f6499e30f44632b29496afbd2dd41f
Sasha Gofman <gofmanaa@gmail.com>
2017-07-25 17:38:31 +0300

    Create freight_economy.rb

---
+++ app/models/spree/calculator/shipping/fedex/freight_economy.rb
@@ -0,0 +1,11 @@
+module Spree
+  module Calculator::Shipping
+    module Fedex
+      class FreightEconomy < Spree::Calculator::Shipping::Fedex::Base
+        def self.description
+           I18n.t("fedex.freight_economy")
+        end
+      end
+    end
+  end
+end
